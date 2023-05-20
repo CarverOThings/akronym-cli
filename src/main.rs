1ba025de6cf6dce23e0dc3a1bf7b7dd4d9e205d7
CarverOThings <adahywoodcraft@gmail.com>
2023-05-20 13:32:48 +0000

    Expanding the logic on main.rs, added json file, learning about References and Borrowing in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
-use rusqlite::{Connection, Result};
+use std::io;
+use std::file::File;
 
-fn main() -> Result<()> {
-    
-    let conn = Connection::open("akronym.db")?;
+struct Defs{
+    name: String
+        definition: String
+        industry: String
+}
 
+fn main() {
     println!("Thank You For Using Akronym!");
+
+    let mut input = String::new();
 
     loop {
         //Display the main menu
-    println!("1. Read a definition.");      
-    println!("2. Insert a defninition");  
-    println!("3. Quit Akronym");    
-    }   
-    
+        println!("1. Read a definition.");
+        println!("2. Insert a defninition");
+        println!("3. Quit Akronym");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Please choose betweeen options 1, 2 or 3.");
+
+        if input == "3" {
+            break;
+        } else if input == "1"{
+            //TODO somefunction( queary the json file for the definiton.)
+        } else if input == "2"{
+            //TODO somefunction( take name and insert def into a json file)
+        }
+
+        break;
+    }
+}
+
+fn insert_definition() => Ok(Result){
+    //TODO
 }
